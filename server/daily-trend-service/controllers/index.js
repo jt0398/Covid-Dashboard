33bd587717b81d33edf791b777021cc063e95fed
Judy <judys717@hotmail.com>
2020-07-04 05:55:02 -0400

    Added routes and controllers

--- server/daily-trend-service/controllers/index.js
+++ server/daily-trend-service/controllers/index.js
@@ -2,5 +2,9 @@ const db = require("../models");
 var Sequelize = require("sequelize");
 
 module.exports = {
-  getActiveCount: function (req, res) {},
+  getActiveCount: function (req, res) {
+    db.National_Daily_Trend.findAll({})
+      .then((dbModel) => res.json(dbModel))
+      .catch((err) => res.status(22).json(err));
+  },
 };
