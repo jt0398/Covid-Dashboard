09f331d4a8fd31b8090c8c10688d445dbc732740
Judy <judys717@hotmail.com>
2020-07-27 08:47:59 -0400

    Initial client setup

---
+++ client/src/pages/MainPage.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Container from "react-bootstrap/Container";
+
+class MainPage extends Component {
+  render() {
+    return (
+      <Container fluid>
+        <Row>
+          <Col>Main Page</Col>
+        </Row>
+      </Container>
+    );
+  }
+}
+
+export default MainPage;
