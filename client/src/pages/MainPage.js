f77eaba8a7bf42893053e8a4fff4d1e2278bdef0
Judy <judys717@hotmail.com>
2020-07-27 21:22:50 -0400

    Update client

--- client/src/pages/MainPage.js
+++ client/src/pages/MainPage.js
@@ -2,8 +2,27 @@ import React, { Component } from "react";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Container from "react-bootstrap/Container";
+import API from "../utils/API";
 
 class MainPage extends Component {
+  state = {
+    dailyData: [],
+  };
+
+  loadDailyData = () => {
+    API.getDailyCount().then((res) => {
+      this.setState({
+        dailyData: res.data,
+      });
+
+      console.log("Dail Data:" + this.state.dailyData);
+    });
+  };
+
+  componentDidMount() {
+    this.loadDailyData();
+  }
+
   render() {
     return (
       <Container fluid>
