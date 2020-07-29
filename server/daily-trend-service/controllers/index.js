789dbddf7801af9dd957e74d04f1e9ef3bcc07b4
Judy <judys717@hotmail.com>
2020-07-28 22:16:02 -0400

    Update Daily Service Routes

--- server/daily-trend-service/controllers/index.js
+++ server/daily-trend-service/controllers/index.js
@@ -1,8 +1,33 @@
 const db = require("../models");
 
 module.exports = {
-  getActiveCount: function (req, res) {
+  getAllCount: function (req, res) {
     db.National_Daily_Trend.findAll({})
+      .then((dbModel) => res.json(dbModel))
+      .catch((err) => res.status(22).json(err));
+  },
+  getActiveCount: function (req, res) {
+    db.National_Daily_Trend.findAll({ attributes: [dateReported, active] })
+      .then((dbModel) => res.json(dbModel))
+      .catch((err) => res.status(22).json(err));
+  },
+  getConfirmedCount: function (req, res) {
+    db.National_Daily_Trend.findAll({ attributes: [dateReported, confirmed] })
+      .then((dbModel) => res.json(dbModel))
+      .catch((err) => res.status(22).json(err));
+  },
+  getRecoveredCount: function (req, res) {
+    db.National_Daily_Trend.findAll({ attributes: [dateReported, recovered] })
+      .then((dbModel) => res.json(dbModel))
+      .catch((err) => res.status(22).json(err));
+  },
+  getDeceasedCount: function (req, res) {
+    db.National_Daily_Trend.findAll({ attributes: [dateReported, deceased] })
+      .then((dbModel) => res.json(dbModel))
+      .catch((err) => res.status(22).json(err));
+  },
+  getTestedCount: function (req, res) {
+    db.National_Daily_Trend.findAll({ attributes: [dateReported, tested] })
       .then((dbModel) => res.json(dbModel))
       .catch((err) => res.status(22).json(err));
   },
