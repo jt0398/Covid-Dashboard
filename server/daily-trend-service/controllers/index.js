97cbc5325b38434c56beeddefba56039a8d24b95
Judy <judys717@hotmail.com>
2020-07-29 19:15:00 -0400

    Added graph components

--- server/daily-trend-service/controllers/index.js
+++ server/daily-trend-service/controllers/index.js
@@ -1,33 +1,47 @@
 const db = require("../models");
+const momemt = require("moment");
 
 module.exports = {
   getAllCount: function (req, res) {
-    db.National_Daily_Trend.findAll({})
+    db.National_Daily_Trend.findAll({ order: [["dateReported", "DESC"], where: {
+      start_datetime: {
+        [Op.gte]: moment().subtract(7, 'days').toDate()
+      }
+    })
       .then((dbModel) => res.json(dbModel))
       .catch((err) => res.status(22).json(err));
   },
   getActiveCount: function (req, res) {
-    db.National_Daily_Trend.findAll({ attributes: [dateReported, active] })
+    db.National_Daily_Trend.findAll({
+      attributes: ["dateReported", "active"],
+      order: [["dateReported", "DESC"]],
+    })
       .then((dbModel) => res.json(dbModel))
       .catch((err) => res.status(22).json(err));
   },
   getConfirmedCount: function (req, res) {
-    db.National_Daily_Trend.findAll({ attributes: [dateReported, confirmed] })
+    db.National_Daily_Trend.findAll({
+      attributes: ["dateReported", "confirmed"],
+    })
       .then((dbModel) => res.json(dbModel))
       .catch((err) => res.status(22).json(err));
   },
   getRecoveredCount: function (req, res) {
-    db.National_Daily_Trend.findAll({ attributes: [dateReported, recovered] })
+    db.National_Daily_Trend.findAll({
+      attributes: ["dateReported", "recovered"],
+    })
       .then((dbModel) => res.json(dbModel))
       .catch((err) => res.status(22).json(err));
   },
   getDeceasedCount: function (req, res) {
-    db.National_Daily_Trend.findAll({ attributes: [dateReported, deceased] })
+    db.National_Daily_Trend.findAll({
+      attributes: ["dateReported", "deceased"],
+    })
       .then((dbModel) => res.json(dbModel))
       .catch((err) => res.status(22).json(err));
   },
   getTestedCount: function (req, res) {
-    db.National_Daily_Trend.findAll({ attributes: [dateReported, tested] })
+    db.National_Daily_Trend.findAll({ attributes: ["dateReported", "tested"] })
       .then((dbModel) => res.json(dbModel))
       .catch((err) => res.status(22).json(err));
   },
