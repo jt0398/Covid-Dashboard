4e5233b9f0e6ed1e07af014474e01f484968ddd5
Judy <judys717@hotmail.com>
2020-08-09 08:06:24 -0400

    Make button functional

--- client/src/pages/MainPage.js
+++ client/src/pages/MainPage.js
@@ -11,13 +11,41 @@ class MainPage extends Component {
     mode: "C",
   };
 
+  constructor(props) {
+    super(props);
+
+    // This binding is necessary to make `this` work in the callback
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick = (event) => {
+    event.preventDefault();
+    this.setState({ mode: event.currentTarget.getAttribute("data-mode") });
+  };
+
   render() {
+    let mode = this.state.mode;
+
     return (
       <Container>
         <Row>
           <Col className="pt-2 m-5">
-            <Button className="h3 p-3 mr-2">Cumulative</Button>
-            <Button className="h3 p-3">Daily</Button>
+            <Button
+              className="h3 p-3 mr-2"
+              variant={mode === "C" ? "secondary" : "primary"}
+              onClick={this.handleClick}
+              data-mode="C"
+            >
+              Cumulative
+            </Button>
+            <Button
+              className="h3 p-3"
+              onClick={this.handleClick}
+              data-mode="D"
+              variant={mode === "D" ? "secondary" : "primary"}
+            >
+              Daily
+            </Button>
           </Col>
         </Row>
         <Row>
